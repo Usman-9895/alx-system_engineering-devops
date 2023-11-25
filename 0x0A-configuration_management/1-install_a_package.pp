982ed80bc51944f45f33c667e584dce0b6c00573
Aya-oba <mbackeousmane31@gmail.com>
2023-11-25 00:36:44 +0000

    Configuration management

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,15 @@
-# Installs puppet-lint v2.1.0.
+# 1-install_a_package.pp
 
-package { 'puppet-lint':
+class { 'python':
+  version => 'system',
+}
+
+package { 'python3-pip':
+  ensure => installed,
+}
+
+package { 'Flask':
   ensure   => '2.1.0',
-  provider => 'gem'
+  provider => 'pip3',
+  require  => Package['python3-pip'],
 }
