795e7f3cd72bb2e4e058ebcd4a25fd4172ce9427
Usman-9895 <mbackeousmane31@gmail.com>
2024-04-14 01:00:58 +0000

    0x17. Web stack debugging #3

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,36 @@
-# Fixes bad "phpp" extensions to "php" in "wp-settings.php".
+# 0-strace_is_your_friend.pp
+class apache_fix {
+  $target_file = '/var/www/html/wp-config.php'
+  $template_content = @(END)
+  <?php
+  // Fichier de configuration wp-config.php exemple pour WordPress
+  define('DB_NAME', 'nom_de_la_base_de_donnees_ici');
+  define('DB_USER', 'nom_utilisateur_ici');
+  define('DB_PASSWORD', 'mot_de_passe_ici');
+  define('DB_HOST', 'localhost');
+  define('DB_CHARSET', 'utf8');
+  define('DB_COLLATE', '');
+  /* Ajoutez des clés uniques et des sels */
+  define('AUTH_KEY',         'mettez votre phrase unique ici');
+  define('SECURE_AUTH_KEY',  'mettez votre phrase unique ici');
+  define('LOGGED_IN_KEY',    'mettez votre phrase unique ici');
+  define('NONCE_KEY',        'mettez votre phrase unique ici');
+  define('AUTH_SALT',        'mettez votre phrase unique ici');
+  define('SECURE_AUTH_SALT', 'mettez votre phrase unique ici');
+  define('LOGGED_IN_SALT',   'mettez votre phrase unique ici');
+  define('NONCE_SALT',       'mettez votre phrase unique ici');
+  ?>
+  END
 
-exec {'fix-wordpress':
-	command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
-	path	=> '/usr/local/bin/:/bin/'
+  file { $target_file:
+    ensure  => file,
+    content => $template_content,
+    owner   => 'www-data',
+    group   => 'www-data',
+    mode    => '0644',
+    require => Package['apache2'],
+  }
 }
+
+include apache_fix
+
