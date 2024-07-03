b4ac10832e0dd6ae06b5de18666a528311219fe1
Usman-9895 <mbackeousmane31@gmail.com>
2024-07-03 22:22:31 +0000

    Fichier 1 ;odifier

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,15 +1,14 @@
 # 1-install_a_package.pp
 
-class { 'python':
-  version => 'system',
-}
-
+# Ensure pip3 is installed
 package { 'python3-pip':
   ensure => installed,
 }
 
+# Install Flask version 2.1.0 using pip3
 package { 'Flask':
   ensure   => '2.1.0',
   provider => 'pip3',
   require  => Package['python3-pip'],
 }
+
